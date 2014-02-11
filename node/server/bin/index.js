e7f5017b0672bf853271a9598ca293014b0eeb56
Bryan Barnard <bmbarnard@gmail.com>
2014-02-10 22:31:59 -0600

    update methods adding CJ render and add'l options to routes

--- node/server/bin/index.js
+++ node/server/bin/index.js
@@ -2,6 +2,7 @@
 var util = require('util')
 var restify = require('../node_modules/restify');
 var items = require('./data.js').friends;
+var bunyan = require('../node_modules/bunyan');
 var env_port = 3000;
 var path = '';
 var base = '';
@@ -11,10 +12,12 @@ var URI = "http://localhost:3000";
 
 restify.defaultResponseHeaders = false; // disable default headers altogether
 
+
 var sendInvalidRequestError = function (req, res, next) {
     res.contentType = 'text/plain';
     res.status(401);
     res.send('we only accept content type ' + mType);
+    next();
 };
 
 var renderCJ = function () {
@@ -39,31 +42,56 @@ var renderCJ = function () {
 var respondDefault = function (req, res, next) {
     if (req.accepts(mType)) {
         renderCJ();
-        var body = JSON.stringify(cj, null, '\t');
+        var body = cj;
+
+        res.contentType = mType;
+        res.status(200);
+        res.send(body);
+        next();
+
+        /*
         res.writeHead(200, {
             'Content-Length': Buffer.byteLength(body),
             'Content-Type': mType
         });
         res.end(body);
+        */
     } else {
         sendInvalidRequestError(req, res, next);
+        next();
     }
 };
 
 //[GET]
 var respondMovies = function (req, res, next) {
-    req.accepts(mType);
-    res.contentType = 'application/json';
-    res.status(200);
-    res.send({movie: 'world'});
+    if (req.accepts(mType)) {
+        renderCJ();
+        var body = JSON.stringify(cj, null, '\t');
+
+        res.contentType = mType;
+        res.status(200);
+        res.send(body);
+        next();
+    } else {
+        sendInvalidRequestError(req, res, next);
+        next();
+    }
 };
 
 //[GET]
 var respondActors = function (req, res, next) {
-    req.accepts(mType);
-    res.contentType = 'application/json';
-    res.status(200);
-    res.send({actors: 'world'});
+    if (req.accepts(mType)) {
+        renderCJ();
+        var body = JSON.stringify(cj, null, '\t');
+
+        res.contentType = mType;
+        res.status(200);
+        res.send(body);
+        next();
+    } else {
+        sendInvalidRequestError(req, res, next);
+        next();
+    }
 };
 
 //* RESPOND NOT FOUND
@@ -72,34 +100,43 @@ var respondNotFound = function (req, res, next) {
     res.contentType = 'text/plain';
     res.status(404);
     res.send('invalid url requested');
+    next();
 };
 
 //declare server
 var server = restify.createServer({
-
-    //add formatter
     formatters: {
         'application/vnd.collection+json; q=0.9': function formatCJ(req, res, body) {
-            if (body instanceof Error)
-                return body.stack;
+            if (body instanceof Error) {
+                res.statusCode = body.statusCode || 500;
+            }
 
-            if (Buffer.isBuffer(body))
+            if (Buffer.isBuffer(body)) {
                 return body.toString('base64');
+            }
 
-            var data = JSON.stringify(body);
+            var data = JSON.stringify(body, null, '\t');
             res.setHeader('Content-Length', Buffer.byteLength(data));
-            return util.inspect(body);
+            return (data);
         }
     }
 });
 
-
 //routes
-server.get('/', respondDefault);
+server.get({path: '/', version: '0.0.0', name: 'billboard'}, respondDefault);
 server.head('/', respondDefault);
 server.get('/movies', respondMovies) ;
 server.get('/actors', respondActors);
+
+//event handlers
 server.on('NotFound', respondNotFound);
+
+server.on('after', restify.auditLogger({
+    log: bunyan.createLogger({
+        name: 'audit',
+        stream: process.stdout
+    })
+}));
 
 //server properties
 server.name = 'node_restify';
@@ -108,7 +145,6 @@ server.name = 'node_restify';
 server.listen(env_port, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
-
 
 // sample collection object
 /*
