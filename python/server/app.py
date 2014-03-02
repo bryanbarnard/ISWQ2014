6bb1b8d237cfe4c0b68d9d0ea7151524c3a8714b
Bryan Barnard <bmbarnard@gmail.com>
2014-03-02 17:16:57 +0000

    updates to handle json

--- python/server/app.py
+++ python/server/app.py
@@ -1,34 +1,18 @@
-
-
 from bottle import Bottle, run, route, abort, redirect, request, response
+from json import dumps
 
 # application
 app = Bottle()
 
 # routes
-
 # default
 @app.route('/')
-def default():
+def default_route():
     abort(404, "not found")
 
-
 # billboard route
 @app.route('/api')
-def billboard():
-    # check accepts and content type
-    # content_type = request.headers.get('Content-Type')
-    accept = request.headers.get('Accept')
-
-    if 'application/vnd.collection+json' in accept:
-        response.set_header('Content-Type', 'application/vnd.collection+json')
-    else:
-        response.set_header('Content-Type', 'application/json')
-    return "billboard"
-
-# billboard route
-@app.route('/api')
-def billboard():
+def billboard_route():
     # check accepts and content type
     # content_type = request.headers.get('Content-Type')
     accept = request.headers.get('Accept')
