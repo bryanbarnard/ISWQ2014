8cac812cb68d551a57de9866f050050509f89f29
Bryan Barnard <bmbarnard@gmail.com>
2014-03-01 23:00:02 -0600

    adding checks for content type and accepts on routes

--- python/server/app.py
+++ python/server/app.py
@@ -17,9 +17,27 @@ def default():
 @app.route('/api')
 def billboard():
     # check accepts and content type
+    # content_type = request.headers.get('Content-Type')
+    accept = request.headers.get('Accept')
 
-    # return billboard
+    if 'application/vnd.collection+json' in accept:
+        response.set_header('Content-Type', 'application/vnd.collection+json')
+    else:
+        response.set_header('Content-Type', 'application/json')
     return "billboard"
 
+# billboard route
+@app.route('/api')
+def billboard():
+    # check accepts and content type
+    # content_type = request.headers.get('Content-Type')
+    accept = request.headers.get('Accept')
 
+    if 'application/vnd.collection+json' in accept:
+        response.set_header('Content-Type', 'application/vnd.collection+json')
+    else:
+        response.set_header('Content-Type', 'application/json')
+    return "billboard"
+
+# main
 run(app, host='localhost', port=8080, debug=True)
